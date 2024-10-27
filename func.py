40846588cf0a1cdaedc805d9a3735a0d728c33cc
HorekiSun <mankevychdima@gmail.com>
2024-10-27 16:35:28 +0200

    REG-005: add git ignore, add more functionals

--- func.py
+++ func.py
@@ -1,5 +1,7 @@
+import os
 from mp3 import  Conventer
 from audiobook import Book_Menu
+import webbrowser
 def title():
     print(" █████╗ ██████╗ ███╗   ███╗██╗███╗   ██╗███████╗ ██████╗")
     print("██╔══██╗██╔══██╗████╗ ████║██║████╗  ██║██╔════╝██╔════╝")
@@ -63,6 +65,13 @@ def FastOpen_Menu():
     print("                 ║   2. Sites         ║                  ")
     print("                 ║   3. Back          ║                  ")
     print("                 ╚════════════════════╝                  ")
+    Fast_choise = input("")
+    if Fast_choise == "1":
+        FastOpen_App()
+    elif Fast_choise == "2":
+        FastOpen_Sites()
+    elif Fast_choise == "3":
+        menu()
 
 
 def Network_Menu():
@@ -83,3 +92,45 @@ def About_Menu():
     print("                 ║   Version : 1.0.0        ║                  ")
     print("                 ║   1. Back                ║                  ")
     print("                 ╚══════════════════════════╝                  ")
+
+def FastOpen_App():
+    Putty = "Apps\putty.exe"
+    AnyDesk = "Apps\AnyDesk.exe"
+    title()
+    print("                 ╔══════════════════════════╗                  ")
+    print("                 ║   1. Putty               ║                  ")
+    print("                 ║   2. Anydesk             ║                  ")
+    print("                 ║   3. Back                ║                  ")
+    print("                 ╚══════════════════════════╝                  ")
+    FastOpen_Choise = input("")
+    if FastOpen_Choise == "1":
+        os.startfile(Putty)
+    if FastOpen_Choise == "2":
+        os.startfile(AnyDesk)
+    if FastOpen_Choise == "3":
+        menu()
+
+def FastOpen_Sites():
+    title()
+    print("                 ╔══════════════════════════╗                  ")
+    print("                 ║   1. Virus Total         ║                  ")
+    print("                 ║   2. Habr                ║                  ")
+    print("                 ║   3. Stack Overflow      ║                  ")
+    print("                 ║   4. Router Panel        ║                  ")
+    print("                 ║   5. Back                ║                  ")
+    print("                 ╚══════════════════════════╝                  ")
+    FastSite_Choise = input("")
+    if FastSite_Choise == "1":
+        VirusTotal = 'https://www.virustotal.com'
+        webbrowser.open(VirusTotal)
+    elif FastSite_Choise == "2":
+        Habr = 'https://habr.com/ru/articles/'
+        webbrowser.open(Habr)
+    elif FastSite_Choise == "3":
+        StackOverflow = 'https://stackoverflow.com/'
+        webbrowser.open(StackOverflow)
+    elif FastSite_Choise == "4":
+        Router = "http://192.168.0.1"
+        webbrowser.open(Router)
+    elif FastSite_Choise == "5":
+        menu()

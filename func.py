17534744c16f99916d4a1f1efa618f40b660f535
HorekiSun <mankevychdima@gmail.com>
2024-10-27 17:09:58 +0200

    REG-007: add fade to program

--- func.py
+++ func.py
@@ -1,21 +1,22 @@
 from password import  Password_Menu; from qrcodegen import Code_Menu; from audiobook import Book_Menu; from audiobook import Book_Menu; from mp3 import  Conventer; import os, webbrowser
+from pystyle import Colors, Colorate
 def title():
-    print(" █████╗ ██████╗ ███╗   ███╗██╗███╗   ██╗███████╗ ██████╗")
-    print("██╔══██╗██╔══██╗████╗ ████║██║████╗  ██║██╔════╝██╔════╝")
-    print("███████║██║  ██║██╔████╔██║██║██╔██╗ ██║█████╗  ██║  ███╗")
-    print("██╔══██║██║  ██║██║╚██╔╝██║██║██║╚██╗██║██╔══╝  ██║   ██║")
-    print("██║  ██║██████╔╝██║ ╚═╝ ██║██║██║ ╚████║███████╗╚██████╔╝")
-    print("╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝ ╚═════╝ ")
+    print(Colorate.Horizontal(Colors.cyan_to_blue, " █████╗ ██████╗ ███╗   ███╗██╗███╗   ██╗███████╗ ██████╗", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue, "██╔══██╗██╔══██╗████╗ ████║██║████╗  ██║██╔════╝██╔════╝", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue, "███████║██║  ██║██╔████╔██║██║██╔██╗ ██║█████╗  ██║  ███╗", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue, "██╔══██║██║  ██║██║╚██╔╝██║██║██║╚██╗██║██╔══╝  ██║   ██║", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue, "██║  ██║██████╔╝██║ ╚═╝ ██║██║██║ ╚████║███████╗╚██████╔╝", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue, "╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝ ╚═════╝ ", 1))
 
 def menu():
     title()
-    print("                 ╔════════════════════╗                  ")
-    print("                 ║   1. Convertors    ║                  ")
-    print("                 ║   2. Fast Opening  ║                  ")
-    print("                 ║   3. Generators    ║                  ")
-    print("                 ║   4. Network       ║                  ")
-    print("                 ║   5. About         ║                  ")
-    print("                 ╚════════════════════╝                  ")
+    print(Colorate.Horizontal(Colors.cyan_to_blue, "                 ╔════════════════════╗                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue, "                 ║   1. Convertors    ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue, "                 ║   2. Fast Opening  ║                  ", 2))
+    print(Colorate.Horizontal(Colors.cyan_to_blue, "                 ║   3. Generators    ║                  ", 2))
+    print(Colorate.Horizontal(Colors.cyan_to_blue, "                 ║   4. Network       ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue, "                 ║   5. About         ║                  ", 3))
+    print(Colorate.Horizontal(Colors.cyan_to_blue, "                 ╚════════════════════╝                  ", 1))
     option = input("")
     if option == "1":
         Convertors_Menu()
@@ -30,11 +31,11 @@ def menu():
 
 def Convertors_Menu():
         title()
-        print("                 ╔════════════════════╗                  ")
-        print("                 ║   1. MP4           ║                  ")
-        print("                 ║   2. Audiobook     ║                  ")
-        print("                 ║   3. Back          ║                  ")
-        print("                 ╚════════════════════╝                  ")
+        print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ╔════════════════════╗                  ", 1))
+        print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   1. MP4           ║                  ", 1))
+        print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   2. Audiobook     ║                  ", 1))
+        print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   3. Back          ║                  ", 1))
+        print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ╚════════════════════╝                  ", 1))
         activity_choise = input("")
         if activity_choise == "1":
             Conventer()
@@ -46,13 +47,13 @@ def Convertors_Menu():
 
 def Generator_Menu():
     title()
-    print("                 ╔════════════════════╗                  ")
-    print("                 ║   1. Password      ║                  ")
-    print("                 ║   2. QR-Code       ║                  ")
-    print("                 ║   3. Email         ║                  ")
-    print("                 ║   4. IP            ║                  ")
-    print("                 ║   5. Back          ║                  ")
-    print("                 ╚════════════════════╝                  ")
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ╔════════════════════╗                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   1. Password      ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   2. QR-Code       ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   3. Email         ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   4. IP            ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   5. Back          ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ╚════════════════════╝                  ", 1))
     Generator_Choise = input("")
     if Generator_Choise == "1":
         Password_Menu()
@@ -65,11 +66,11 @@ def Generator_Menu():
 
 def FastOpen_Menu():
     title()
-    print("                 ╔════════════════════╗                  ")
-    print("                 ║   1. Apps          ║                  ")
-    print("                 ║   2. Sites         ║                  ")
-    print("                 ║   3. Back          ║                  ")
-    print("                 ╚════════════════════╝                  ")
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ╔════════════════════╗                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   1. Apps          ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   2. Sites         ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   3. Back          ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ╚════════════════════╝                  ", 1))
     Fast_choise = input("")
     if Fast_choise == "1":
         FastOpen_App()
@@ -81,12 +82,12 @@ def FastOpen_Menu():
 
 def Network_Menu():
     title()
-    print("                 ╔════════════════════╗                  ")
-    print("                 ║   1. Check IP      ║                  ")
-    print("                 ║   2. IP Scanner    ║                  ")
-    print("                 ║   3. Active Ports  ║                  ")
-    print("                 ║   4. Back          ║                  ")
-    print("                 ╚════════════════════╝                  ")
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ╔════════════════════╗                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   1. Check IP      ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   2. IP Scanner    ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   3. Active Ports  ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   4. Back          ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ╚════════════════════╝                  ", 1))
     Network_Choise = input("")
     if Network_Choise == "4":
         menu()
@@ -94,12 +95,12 @@ def Network_Menu():
 
 def About_Menu():
     title()
-    print("                 ╔══════════════════════════╗                  ")
-    print("                 ║   GitHub : TheDmitryY    ║                  ")
-    print("                 ║   Telegram : HorekiSun   ║                  ")
-    print("                 ║   Version : 1.0.0        ║                  ")
-    print("                 ║   1. Back                ║                  ")
-    print("                 ╚══════════════════════════╝                  ")
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ╔══════════════════════════╗                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   GitHub : TheDmitryY    ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   Telegram : HorekiSun   ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   Version : 1.0.0        ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   1. Back                ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ╚══════════════════════════╝                  ", 1))
     About_Choise = input("")
     if About_Choise == "1":
         menu()
@@ -108,11 +109,11 @@ def FastOpen_App():
     Putty = "Apps\putty.exe"
     AnyDesk = "Apps\AnyDesk.exe"
     title()
-    print("                 ╔══════════════════════════╗                  ")
-    print("                 ║   1. Putty               ║                  ")
-    print("                 ║   2. Anydesk             ║                  ")
-    print("                 ║   3. Back                ║                  ")
-    print("                 ╚══════════════════════════╝                  ")
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ╔══════════════════════════╗                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   1. Putty               ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   2. Anydesk             ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   3. Back                ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ╚══════════════════════════╝                  ", 1))
     FastOpen_Choise = input("")
     if FastOpen_Choise == "1":
         os.startfile(Putty)
@@ -123,13 +124,13 @@ def FastOpen_App():
 
 def FastOpen_Sites():
     title()
-    print("                 ╔══════════════════════════╗                  ")
-    print("                 ║   1. Virus Total         ║                  ")
-    print("                 ║   2. Habr                ║                  ")
-    print("                 ║   3. Stack Overflow      ║                  ")
-    print("                 ║   4. Router Panel        ║                  ")
-    print("                 ║   5. Back                ║                  ")
-    print("                 ╚══════════════════════════╝                  ")
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ╔══════════════════════════╗                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   1. Virus Total         ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   2. Habr                ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   3. Stack Overflow      ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   4. Router Panel        ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ║   5. Back                ║                  ", 1))
+    print(Colorate.Horizontal(Colors.cyan_to_blue,"                 ╚══════════════════════════╝                  ", 1))
     FastSite_Choise = input("")
     if FastSite_Choise == "1":
         VirusTotal = 'https://www.virustotal.com'
